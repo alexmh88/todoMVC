167a62b4560d4f410074d6b63a37701caa0f7a4b
Simon Garding <simon.garding1@gmail.com>
2022-02-24 14:41:36 +0100

    new todo

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 const todoInput = document.querySelector('.todo-input');
 const todoList = document.querySelector('.todo-list');
+
+todoInput.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        addTodo
+    }
+});
+
+function addTodo(event){
+    event.preventDefault();
+    //div todo
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    todoList.appendChild(todoDiv);
+}
